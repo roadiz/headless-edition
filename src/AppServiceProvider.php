3e1a6dad1f1cf37bffdb150429269ea5c6df92e3
Ambroise Maupate <ambroise@rezo-zero.com>
2021-01-18 23:20:20 +0100

    Initial commit

---
+++ src/AppServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+
+use Pimple\Container;
+
+class AppServiceProvider implements \Pimple\ServiceProviderInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function register(Container $pimple)
+    {
+        // TODO: Implement register() method.
+    }
+}
