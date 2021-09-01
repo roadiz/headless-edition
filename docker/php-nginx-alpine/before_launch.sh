18b25a506572f6e3bc7e40a0f591c4e20d8362ad
Ambroise Maupate <ambroise@rezo-zero.com>
2021-09-01 14:49:43 +0200

    Sleep to wait for database

--- docker/php-nginx-alpine/before_launch.sh
+++ docker/php-nginx-alpine/before_launch.sh
@@ -7,6 +7,9 @@ printenv | sed 's/^\(.*\)$/export \1/g' | grep -E "^export (SYMFONY|APP|ROADIZ|M
 
 /bin/chmod +x /var/www/html/project_env.sh;
 
+# Wait for database to be ready.
+/bin/sleep 5s;
+
 # Uncomment following line to enable automatic migration for your theme at each docker start
 /usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/roadiz migrations:migrate -q --allow-no-migration"
 /usr/bin/sudo -u www-data -- bash -c "/var/www/html/project_env.sh; /var/www/html/bin/roadiz themes:migrate -n src/Resources/config.yml"
